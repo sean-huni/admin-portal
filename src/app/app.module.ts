c6d376193015306e5bc97fec7360be366a72e19e
Kudzai Huni <sean2kay@gmail.com>
2018-12-30 14:19:23 +0200

    Added a new book service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,25 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
-import {FormsModule} from '@angular/forms';
-
 import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-import {AppComponent} from './app.component';
 import {routing} from './app.routing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import 'hammerjs';
-import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {LoginComponent} from './components/login/login.component';
 import {MatGridListModule} from '@angular/material';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 import {LoginService} from './services/login.service';
-import {AddBookComponent} from './components/add-book/add-book.component';
+import {AddBookService} from './services/add-book.service';
 
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+import {LoginComponent} from './components/login/login.component';
+import {AddBookComponent} from './components/add-book/add-book.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +46,8 @@ import {AddBookComponent} from './components/add-book/add-book.component';
     routing
   ],
   providers: [
-    LoginService
+    LoginService,
+    AddBookService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Book} from '../../model/book';
+import {AddBookService} from '../../services/add-book.service';
 
 @Component({
   selector: 'app-add-book',
@@ -7,12 +8,29 @@ import {Book} from '../../model/book';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
-private newBook: Book = new Book();
-private bookAdded: boolean;
+  private newBook: Book = new Book();
+  private bookAdded: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private addBookService: AddBookService) {
   }
 
+  ngOnInit() {
+    this.bookAdded = false;
+    this.newBook.active = true;
+    this.newBook.language = 'english';
+  }
+
+  onSubmit() {
+    this.addBookService.sendBook(this.newBook).subscribe(
+      res => {
+        this.bookAdded = true;
+        this.newBook = new Book();
+        this.newBook.language = 'english';
+
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
